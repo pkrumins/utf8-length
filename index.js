5b06497ebc448d6ba8640ea011cac5ce68854d1e
Jed Schmidt <where@jed.is>
2013-11-29 18:36:09 -0500

    Use @mathiasbynens' astral-safe version
    see https://github.com/substack/utf8-length/issues/2#issuecomment-29539311

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-module.exports = function (str) {
-    var sum = 0;
-    for (var i = 0; i < str.length; i++) {
-        sum += nbytes(str.charCodeAt(i));
-    }
-    return sum;
-};
-
-function nbytes (c) {
-    if (c >= 2048) return 3;
-    if (c >= 128) return 2;
-    return 1;
+module.exports = function(s) {
+  return ~-encodeURI(s).split(/%..|./).length
 }
